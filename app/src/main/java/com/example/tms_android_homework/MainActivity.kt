bb0dbb633d4f924ffa2f3cd7eb8930fb45ab753d
ShukelovichDmitry <mitya.shukelovich@mail.ru>
2025-07-27 19:14:48 +0300

    Урок 20

--- app/src/main/java/com/example/tms_android_homework/MainActivity.kt
+++ app/src/main/java/com/example/tms_android_homework/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.tms_android_homework
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.transition.Visibility
 import com.example.tms_android_homework.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayoutMediator
@@ -19,28 +23,37 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val myAdapter = MyAdapter(this)
-        binding.pager.adapter = myAdapter
-        binding.addFragment.setOnClickListener {
-            if (myAdapter.addFragment()) {
-                binding.pager.setCurrentItem(myAdapter.itemCount - 1, false)
-                binding.tabs.addTab(binding.tabs.newTab().apply { text = "Tab #${myAdapter.itemCount - 1}" })
-                Snackbar.make(binding.root, "Добавлен новый фрагмент", Snackbar.LENGTH_SHORT).show()
-            }
-        }
-        binding.cancel.setOnClickListener {
-            if (myAdapter.deleteLastFragment()) {
-                if (myAdapter.itemCount == binding.pager.currentItem)
-                    binding.pager.setCurrentItem(myAdapter.itemCount - 1, false)
-                binding.tabs.removeTabAt(myAdapter.itemCount)
-                Snackbar.make(binding.root, "Последнее действие отменено ", Snackbar.LENGTH_SHORT).show()
+        val myAdapter = MyAdapter(mutableListOf())
+        binding.list.adapter = myAdapter
+        binding.list.layoutManager = LinearLayoutManager(this)
+
+        myAdapter.registerAdapterDataObserver(object : RecyclerView.AdapterDataObserver() {
+            override fun onItemRangeInserted(positionStart: Int, itemCount: Int) {
+                super.onItemRangeInserted(positionStart, itemCount)
+                if (myAdapter.itemCount > 0) {
+                    binding.noDataText.visibility = View.GONE
+                    binding.list.visibility = View.VISIBLE
+                }
             }
 
-        }
+            override fun onItemRangeRemoved(positionStart: Int, itemCount: Int) {
+                super.onItemRangeRemoved(positionStart, itemCount)
+                if (myAdapter.itemCount == 0) {
+                    binding.noDataText.visibility = View.VISIBLE
+                    binding.list.visibility = View.GONE
+                }
+            }
+        })
 
-        TabLayoutMediator(binding.tabs, binding.pager) { tab, position ->
-            tab.text = "Tab #$position"
-        }.attach()
+        binding.addItem.setOnClickListener {
+            val str = binding.newItemText.text.toString()
+            if (str != "") {
+                myAdapter.addItem(str)
+                Snackbar.make(binding.root, "Добавлен новый элемент", Snackbar.LENGTH_SHORT).show()
+            } else {
+                Snackbar.make(binding.root, "Введите текст в поле для ввода", Snackbar.LENGTH_SHORT).show()
+            }
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
--- app/src/main/java/com/example/tms_android_homework/MyAdapter.kt
+++ app/src/main/java/com/example/tms_android_homework/MyAdapter.kt
@@ -1,31 +1,48 @@
 package com.example.tms_android_homework
 
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import androidx.viewpager2.adapter.FragmentStateAdapter
-import androidx.viewpager2.adapter.FragmentViewHolder
-import java.util.LinkedList
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tms_android_homework.databinding.LayoutItemBinding
+import com.google.android.material.snackbar.Snackbar
 
-class MyAdapter(fragment: FragmentActivity) : FragmentStateAdapter(fragment) {
-    private var fragmentCount = 1
+class MyAdapter(val itemList: MutableList<String>): RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+    class MyViewHolder(private val binding: LayoutItemBinding): RecyclerView.ViewHolder(binding.root) {
+        fun onBind(str: String, removeAction: ()->Unit) {
+            binding.itemText.text = str
+            binding.deleteItem.setOnClickListener {
+                removeAction()
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding = LayoutItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return MyViewHolder(binding)
+    }
 
     override fun getItemCount(): Int {
-        return fragmentCount
+        return itemList.size
     }
 
-    override fun createFragment(position: Int): Fragment {
-        return DynamicFragment(position)
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.onBind(itemList[position], { removeItem(position) })
+
     }
 
-    fun addFragment(): Boolean {
-        fragmentCount++
-        return true
+    fun addItem(str: String) {
+        itemList.add(str)
+        notifyItemInserted(itemList.size - 1)
     }
 
-    fun deleteLastFragment(): Boolean {
-        if (fragmentCount == 1)
-            return false
-        fragmentCount--
-        return true
+    fun removeItem(position: Int) {
+        itemList.removeAt(position)
+        notifyItemRemoved(position)
     }
+
 }
